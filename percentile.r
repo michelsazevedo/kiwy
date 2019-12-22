98499127b59d985ae9419ff6e9c1dc1b2afa4f84
Michel Azevedo <michel.azevedos@gmail.com>
2019-12-22 20:35:08 -0300

    spanner latency test

--- percentile.r
+++ percentile.r
@@ -1,14 +1,22 @@
 dirbase <- "/kiwy/resources/csv"
 files <- unlist(list.files(dirbase))
 
+filename <- paste("/kiwy", "resources", "percentiles", "percentiles.csv", sep="/")
+
 for(i in 1:length(files)) {
   data <- read.csv(paste(dirbase, files[i], sep="/"), header=FALSE)
   names(data) <- c('datetime', 'time', 'count')
 
-  sysTimes <- unlist(data['time'])
+  sysTimes <- round(unlist(data['time'])*1000)
   
-  percentiles <- quantile(sysTimes, prob = c(0.50, 0.75, 0.90, 0.95, 0.99))
-  filename <- paste("/kiwy", "resources", "percentiles", files[i], sep="/")
+  quantiles <- quantile(sysTimes, prob = c(0.50, 0.75, 0.90, 0.95, 0.99))
 
-  write.csv(percentiles, file = filename)
+  throughput <- strsplit(files[i], ".csv")[[1]]
+
+  throughputs <- c(replicate(5, throughput))
+  percentiles <- c("p50", "p75", "p90", "p95", "p99")
+
+  data <- data.frame(throughputs, percentiles, quantiles)
+
+  write.table(data, filename, append = T, col.names=FALSE)
 }
